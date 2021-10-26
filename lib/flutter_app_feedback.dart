48bd29bcfa5dd4213d38cde6f30c65929cc19c6a
Mihir Paldhikar <imihirpaldhikar@gmail.com>
2021-10-26 09:37:34 +0530

    Exported the feedback.screen.dart to make it importable in other projects.

--- lib/flutter_app_feedback.dart
+++ lib/flutter_app_feedback.dart
@@ -23,8 +23,4 @@
 
 library flutter_app_feedback;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export './ui/feedback.screen.dart';
